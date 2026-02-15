1e67b1b50318ab7cc10b9889ddf144ae694f06a2
Nadiar <codxse@gmail.com>
2026-02-15 20:23:46 +0700

    fix: save markdown as value

---
+++ demo/spec/system/admin_articles_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "Admin Articles", type: :system do
+  before do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  end
+
+  it "saves markdown body when creating an article" do
+    visit "/admin/articles/new"
+
+    fill_in "Title", with: "Test Article"
+
+    editor_textarea = find(".editor-textarea", visible: :all)
+    editor_textarea.set("# Hello World")
+
+    click_button "Create Article"
+
+    article = Article.last
+    expect(article.title).to eq("Test Article")
+    expect(article.body).to eq("# Hello World")
+  end
+end
