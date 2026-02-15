479854e15e5c3a424cb1c6773e0021c5e009ca31
Nadiar <codxse@gmail.com>
2026-02-15 17:13:56 +0700

    feat: add gem entry point and version

---
+++ lib/activeadmin_mitosis_editor.rb
@@ -0,0 +1,7 @@
+require "activeadmin_mitosis_editor/version"
+
+module ActiveadminMitosisEditor
+  module Inputs
+    autoload :MitosisEditorInput, "activeadmin_mitosis_editor/inputs/mitosis_editor_input"
+  end
+end
---
+++ lib/activeadmin_mitosis_editor/version.rb
@@ -0,0 +1,3 @@
+module ActiveadminMitosisEditor
+  VERSION = "0.1.0"
+end
