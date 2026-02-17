357760dbcb16dcc12a5cde5c56b6c491a89db834
Nadiar <codxse@gmail.com>
2026-02-17 15:22:37 +0700

    fix: fix initializer

--- lib/activeadmin_mitosis_editor/railtie.rb
+++ lib/activeadmin_mitosis_editor/railtie.rb
@@ -1,7 +1,32 @@
 module ActiveAdminMitosisEditor
   class Engine < Rails::Engine
-    # Asset paths are not registered via Railtie to avoid conflicts with app-level
-    # asset overrides. Applications using this gem should copy the bundled assets
-    # from vendor/assets to their app/assets/stylesheets/mitosis_editor/ directory.
+    # Add gem assets to the asset pipeline
+    # App assets take precedence - gem assets are added at the END
+    # so Rails finds app's version first (if it exists)
+    initializer "activeadmin_mitosis_editor.assets" do
+      Rails.application.config.assets.paths <<
+        root.join("vendor/assets/stylesheets").to_s
+      Rails.application.config.assets.paths <<
+        root.join("vendor/assets/javascripts").to_s
+    end
+
+    initializer "activeadmin_mitosis_editor.formtastic_custom_inputs" do
+      require "activeadmin_mitosis_editor/inputs/mitosis_editor_input"
+
+      Formtastic::Inputs.send(:include, Module.new do
+        def self.included(base)
+          base.extend(ClassMethods)
+        end
+
+        module ClassMethods
+          def custom_input_class(name, class_name)
+            @custom_input_classes ||= {}
+            @custom_input_classes[name.to_sym] = class_name
+          end
+        end
+      end)
+
+      Formtastic::Inputs::MitosisEditorInput = ActiveAdminMitosisEditor::Inputs::MitosisEditorInput
+    end
   end
 end
--- lib/generators/mitosis_editor/styles_generator.rb
+++ lib/generators/mitosis_editor/styles_generator.rb
@@ -9,9 +9,9 @@ module MitosisEditor
     def copy_css_files
       copy_file "mitosis-editor.css",
         "app/assets/stylesheets/mitosis-editor.css"
-      copy_file "theme-light.min.css",
+      copy_file "theme-light.css",
         "app/assets/stylesheets/theme-light.css"
-      copy_file "theme-dark.min.css",
+      copy_file "theme-dark.css",
         "app/assets/stylesheets/theme-dark.css"
     end
 
